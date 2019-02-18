cd382e24cf2fc439cdf33640a529bcd8b066fb7a
Tzu-Yin Hong <lili668668@gmail.com>
2019-02-19 00:00:10 +0800

    Fix document website generating problem

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  ignore: [/\.test\.js$/],
-  only: [/\.js$/]
+  presets: ['@babel/preset-env', '@babel/preset-react']
 }
--- doczrc.js
+++ doczrc.js
@@ -2,5 +2,5 @@ module.exports = {
   title: 'react-component-lib-boilerplate',
   description: 'A boilerplate for react component library',
   port: 5000,
-  dest: 'doc-dist'
+  dest: 'docs'
 }
